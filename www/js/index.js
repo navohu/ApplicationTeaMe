23ee22ead3026d5f6c6b96a9b735f331a1c08d61
Tali <nathalievh95@gmail.com>
2016-02-01 22:41:31 +0000

    Message Start v.0
    Started creating the messaging chat application with  PubNub, jQuery
    Mob, HTML5 and CSS3.

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,43 @@
+/*Creating a PubNub Chat forum for 1:1 messaging*/
+function chat(){
+	var PUBNUB_demo = PUBNUB.init({
+                publish_key: 'pub-c-1d031e2d-de94-4a51-a7af-34e61c1083be',
+                subscribe_key: 'sec-c-NGRmNmFjOTQtYWYxYy00Yzg0LTg5NzAtMDc4MTBhY2M5ZTY4'
+            });
+	var chat = PUBNUB.$('box'), input = PUBNUB.$('input'), channel = 'chat';
+	var now = new Date();
+
+	pubnub.channel({
+		channel: 'my_channel',
+	    message: function(m){console.log(m + now.getHours() + now.getMinutes())},
+	    error: function (error) {
+	      // Handle error here
+	      console.log(JSON.stringify(error));
+	    }
+	});
+
+	PUBNUB.subscribe({
+    channel  : channel,
+    callback : function(text)
+    	{ 
+    		box.innerHTML = (''+text).replace( /[<>]/g, '' ) + '<br>' + box.innerHTML 
+    	}
+	});
+
+	pubnub.publish({
+    channel: 'my_channel',        
+    message: 'Hello from the PubNub Javascript SDK!',
+    callback : function(m){console.log(m)}
+	});
+
+	PUBNUB.bind( 'keyup', input, function(e) {
+    (e.keyCode || e.charCode) === 13 && PUBNUB.publish({
+        channel : channel, message : input.value, x : (input.value='')
+    })
+	});
+}
+
+
 var EnterKey = 13;
 
 $.fn.isBound = function(type, fn) {
@@ -54,6 +94,8 @@ $(document).ready(function() {
 		runBind();
 		$('#main').show();
     
+    chat();
   }}); // end if
 });
+
 
