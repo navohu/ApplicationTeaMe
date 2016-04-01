786c3c8ef3d2df0c51fc213335b9f53184c834fd
Laman <qafqaz9393@mail.ru>
2016-04-01 15:01:24 +0100

    Users per Team

--- www/createTeam1.php
+++ www/createTeam1.php
@@ -1,4 +1,5 @@
 <?php include("createATeam.html");?>
+hello
 
 <?php
 
@@ -6,7 +7,6 @@
    require('db.php');
    session_start();
 
-   // $uname = $_SESSION["uuid"];
 
 
    if (isset($_POST['submit']))
@@ -16,13 +16,22 @@
 
      $team_name = mysql_escape_string($_POST['team_name']);
 
- //     $sql = "INSERT INTO `Teams` (`team_id`, `team_name`, `team_admin`) VALUES (NULL, '$team_name', '$uname')";
- // echo $sql ;
- // mysql_query($sql) or die(mysql_error());
- //   header("Location: http://localhost/Teame_data/ApplicationTeaMe/www/teams.php");
- //   exit();
+     $result = mysql_query("SELECT id FROM Users WHERE uname='$uname'");
+     $value = mysql_fetch_object($result);
+     $user_id = $value->id;
 
-     mysql_query("INSERT INTO `Teams` (`team_id`, `team_name`, `team_admin`) VALUES (NULL, '$team_name', '$uname')") or die(mysql_error());
+     mysql_query("INSERT INTO `Teams` (`team_name`, `team_admin`) VALUES ('$team_name', '$uname')") or die(mysql_error());
+
+     $result_team_id = mysql_query("SELECT team_id FROM Teams WHERE team_name='$team_name'");
+     $value_team_id = mysql_fetch_object($result_team_id);
+     $team_id = $value_team_id->team_id;
+
+     // echo 'this is the team id'. $team_id . "\n";
+
+
+     mysql_query("INSERT INTO `Users_per_Team` (`user_id`, `team_id`) VALUES ('$user_id', '$team_id')") or die(mysql_error());
+    $_SESSION["teamuuid"] = $team_id;
+
      header("Location: http://localhost/Teame_data/ApplicationTeaMe/www/teams.html");
      exit();
 
