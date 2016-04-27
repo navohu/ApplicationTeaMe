bdebba2d14aa69d01de92598b56ea829d8a30f62
Tali <nathalievh95@gmail.com>
2016-04-27 13:38:38 +0100

    Removed unused files

--- Login/TeaMe_2/controllers/base/head.php
+++
@@ -1,8 +0,0 @@
-    <?php include 'controllers/base/meta-tags.php' ?>
-
-    <title>TeaMe</title>
-
-    <?php include 'controllers/base/javascript.php' ?>
-    <?php include 'controllers/base/css.php' ?>
-
-    <?php include 'controllers/base/font.php' ?>
